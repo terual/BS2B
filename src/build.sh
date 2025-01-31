98bc4afae16ab2f1e4af96ec8b7edb30f7151f42
Bart Lauret <bblauret@gmail.com>
2025-01-31 10:25:33 +0100

    Drop autotools build system and use simple Makefile

--- src/build.sh
+++ src/build.sh
@@ -11,13 +11,12 @@ patch -p0 < fix-stderr.patch
 patch -p0 < missing_math_lib.patch
 patch -p0 < remove_AC_FUNC_MALLOC.patch
 
-cd "libbs2b-3.1.0"
-docker run -t --rm -u "${UID}" -v "${PWD}:${PWD}" -w "${PWD}" \
-	bs2b \
-	sh -c "LIBS=-lm ./configure --enable-static --disable-shared && make"
-
-cp src/bs2bconvert ../../Bin/x86_64-linux
-cd ..
+cd "libbs2b-3.1.0/src"
+rm Makefile* bs2bstream.c
+cp ../../Makefile .
+docker run -t --rm -u "${UID}" -v "${PWD}:${PWD}" -w "${PWD}" bs2b sh -c "make"
+cp bs2bconvert ../../../Bin/x86_64-linux
+cd ../..
 
 # Cross-building for aarch64-linux
 docker build -f Dockerfile.arm64 . -t bs2b-arm64
@@ -27,12 +26,12 @@ patch -p0 < fix-stderr.patch
 patch -p0 < missing_math_lib.patch
 patch -p0 < remove_AC_FUNC_MALLOC.patch
 
-cd "libbs2b-3.1.0"
-docker run -t --rm -u "${UID}" -v "${PWD}:${PWD}" -w "${PWD}" \
-	bs2b-arm64 \
-	sh -c "autoreconf -vfi && LIBS=-lm ./configure --build=x86_64-linux-gnu --host=aarch64-linux-gnu --target=aarch64-linux-gnu --enable-static --disable-shared && make"
-cp src/bs2bconvert ../../Bin/aarch64-linux
-cd ..
+cd "libbs2b-3.1.0/src"
+rm Makefile* bs2bstream.c
+cp ../../Makefile .
+docker run -t --rm -u "${UID}" -v "${PWD}:${PWD}" -w "${PWD}" bs2b-arm64 sh -c "make CC=aarch64-linux-gnu-gcc"
+cp bs2bconvert ../../../Bin/aarch64-linux
+cd ../..
 
 # Cross-building for armhf-linux
 docker build -f Dockerfile.armhf . -t bs2b-armhf
@@ -42,11 +41,12 @@ patch -p0 < fix-stderr.patch
 patch -p0 < missing_math_lib.patch
 patch -p0 < remove_AC_FUNC_MALLOC.patch
 
-cd "libbs2b-3.1.0"
-docker run -t --rm -u "${UID}" -v "${PWD}:${PWD}" -w "${PWD}" \
-	bs2b-armhf \
-	sh -c "autoreconf -vfi && LIBS=-lm ./configure --build=x86_64-linux-gnu --host=arm-linux-gnueabihf --target=arm-linux-gnueabihf --enable-static --disable-shared && make"
+cd "libbs2b-3.1.0/src"
+rm Makefile* bs2bstream.c
+cp ../../Makefile .
+docker run -t --rm -u "${UID}" -v "${PWD}:${PWD}" -w "${PWD}" bs2b-armhf sh -c "make CC=arm-linux-gnueabihf-gcc"
+cp bs2bconvert ../../../Bin/armhf-linux
+cd ../..
 
-cp src/bs2bconvert ../../Bin/armhf-linux
-cd ..
+# Cleaning up
 rm -rf "libbs2b-3.1.0"
